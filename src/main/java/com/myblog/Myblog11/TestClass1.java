3c8143f4f533fd4edab7f99bffe57b7cc6fd1a11
Admin <faheemsiddiqu786@gmail.com>
2024-02-10 12:54:00 +0530

    faheemsiddiquiwebsite Tables are created Api Created 4 jan 2024 Stream APi

---
+++ src/main/java/com/myblog/Myblog11/TestClass1.java
@@ -0,0 +1,41 @@
+package com.myblog.Myblog11;
+
+public class TestClass1 {
+    public static void main(String[] args) {
+
+
+//        List<String> numbers= Arrays.asList("mike","adam","mike","Stallin");
+//        List<String>data1=numbers.stream().filter(s->s.startsWith("a")).collect(Collectors.toList());
+//        List<String>data2=numbers.stream().filter(s->s.equals("mike")).collect(Collectors.toList());
+//        List<String>data3=numbers.stream().filter(s->s.endsWith("n")).collect(Collectors.toList());
+//        System.out.println(data1);
+//        System.out.println(data2);
+//        System.out.println(data3);
+
+
+
+//        List<Integer>numbers= Arrays.asList(10,12,1,5,16,19,20);
+//        List<Integer>oddNumbers=numbers.stream().filter(n->n%2!=0).collect(Collectors.toList());
+//        System.out.println(oddNumbers);
+
+
+
+//        List<Integer> Numbers = Arrays.asList(10, 12, 1, 5, 16, 19, 20);
+//
+//       List<Integer>evenNumber= Numbers.stream().filter(n->n%2==0).collect(Collectors.toList());
+//        System.out.println(evenNumber);
+
+
+//      Predicate<String> val= str->str.equals("mike");
+//
+//        boolean result=val.test("stalin");
+//        System.out.println(result);
+//
+
+//        List<Integer> numbers = Arrays.asList(10, 20, 15, 45, 46, 16);
+//        Predicate<Integer> val= x->x%2==0;
+//       boolean result= val.test(10);
+//        System.out.println(result);
+
+    }
+}
